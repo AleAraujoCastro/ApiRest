007af9ac7f80e1ad504024165137779d9e3f8246
Alexandre Rodolpho <alearaujocastro@hotmail.com>
2022-07-28 20:52:47 -0300

    Alterei o ProdutoRepository, e criei o ProdutoResource com Get, Post.

---
+++ ApiRest/src/main/java/com/apirest/resources/ProdutoResource.java
@@ -0,0 +1,47 @@
+package com.apirest.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.apirest.models.Produto;
+import com.apirest.repository.ProdutoRepository;
+
+@RestController
+@RequestMapping("/api")
+public class ProdutoResource {
+	
+	@Autowired
+	ProdutoRepository pr;
+	
+	
+	@GetMapping("/produtos")
+	public List<Produto> listaProdutos(){		
+		return pr.findAll();
+	}
+	
+	
+	@GetMapping("/produto/{id}")
+	public Produto produtoId(@PathVariable(value="id") long id){		 
+		return pr.findById(id);
+	}
+	
+	
+	@PostMapping("/produto")
+	public Produto salvarProduto(@RequestBody Produto produto) {		
+		return pr.save(produto);
+	}
+	
+	
+	
+	
+	
+	
+
+}
