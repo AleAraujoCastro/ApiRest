e677301255813ddd052779b68d39088bb1644df7
Alexandre Rodolpho <alearaujocastro@hotmail.com>
2022-07-29 19:14:58 -0300

    deploy no heroku

--- ApiRest/src/main/java/com/apirest/resources/ProdutoResource.java
+++ ApiRest/src/main/java/com/apirest/resources/ProdutoResource.java
@@ -3,6 +3,7 @@ package com.apirest.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,8 +16,13 @@ import org.springframework.web.bind.annotation.RestController;
 import com.apirest.models.Produto;
 import com.apirest.repository.ProdutoRepository;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 @RestController
 @RequestMapping("/api")
+@Api(value="API REST Produtos")
+@CrossOrigin(origins="*")
 public class ProdutoResource {
 	
 	@Autowired
@@ -24,28 +30,33 @@ public class ProdutoResource {
 	
 	
 	@GetMapping("/produtos")
+	@ApiOperation(value="Retorna uma Lista de Produtos")
 	public List<Produto> listaProdutos(){		
 		return pr.findAll();
 	}
 	
 	
 	@GetMapping("/produto/{id}")
+	@ApiOperation(value="Retorna um Produto específico")
 	public Produto produtoId(@PathVariable(value="id") long id){		 
 		return pr.findById(id);
 	}
 	
 	
 	@PostMapping("/produto")
+	@ApiOperation(value="Adiciona um Produto na lista")
 	public Produto salvarProduto(@RequestBody Produto produto) {		
 		return pr.save(produto);
 	}
 	
 	@PutMapping("/produto")
+	@ApiOperation(value="Atualiza um Produto")
 	public Produto atualizarProduto(@RequestBody Produto produto) {		
 		return pr.save(produto);
 	}
 	
 	@DeleteMapping("/produto/{id}")
+	@ApiOperation(value="Deleta um Produto específico")
 	public void detelarProduto(@RequestBody Produto produto) {		
 		pr.delete(produto);
 	}
