46a2bd1fa8225716a075d85ca15cfe15bd330c35
Alexandre Rodolpho <alearaujocastro@hotmail.com>
2022-07-28 22:33:44 -0300

    Adicionei os métodos Put e Delete.

--- ApiRest/src/main/java/com/apirest/resources/ProdutoResource.java
+++ ApiRest/src/main/java/com/apirest/resources/ProdutoResource.java
@@ -3,9 +3,11 @@ package com.apirest.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,6 +38,16 @@ public class ProdutoResource {
 	@PostMapping("/produto")
 	public Produto salvarProduto(@RequestBody Produto produto) {		
 		return pr.save(produto);
+	}
+	
+	@PutMapping("/produto")
+	public Produto atualizarProduto(@RequestBody Produto produto) {		
+		return pr.save(produto);
+	}
+	
+	@DeleteMapping("/produto/{id}")
+	public void detelarProduto(@RequestBody Produto produto) {		
+		pr.delete(produto);
 	}
 	
 	
