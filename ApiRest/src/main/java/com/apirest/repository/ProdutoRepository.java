b430ae8f5dc84f6d9d6f52b0786a45ec6d6517d2
Alexandre Rodolpho <alearaujocastro@hotmail.com>
2022-07-29 21:00:50 -0300

    add a assinatura @Repository na classe ProdutoRepository

--- ApiRest/src/main/java/com/apirest/repository/ProdutoRepository.java
+++ ApiRest/src/main/java/com/apirest/repository/ProdutoRepository.java
@@ -1,9 +1,11 @@
 package com.apirest.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.apirest.models.Produto;
 
+@Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Long>{
 	
 	Produto findById (long id);
